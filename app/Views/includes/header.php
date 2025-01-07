ac0a7930767df17ad8407245ec3d235e6b89faaf
dev-shubham20 <dev.shubhamagrawal@gmail.com>
2025-01-08 00:25:48 +0530

    upload code

---
+++ app/Views/includes/header.php
@@ -0,0 +1,35 @@
+<head>
+      <meta charset="UTF-8">
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+      <title>Veatfresh - An online fresh fish,chicken, mutton, fruits & veg shop in Lucknow & kanpur</title>
+      <meta name="keywords" content="fish market near me, fish shop near me, meat shop near me, chicken home delivery near me, raw chicken home delivery near me, chicken shop near me home delivery, fresh fish home delivery near me, online chicken delivery near me, fresh mutton shop near me, raw fish shop near me, best chicken shop near me, best mutton shop near me, fresh chicken home delivery, chicken online delivery near me,raw mutton shop near me, home delivery in Lucknow, home delivery in kanpur" />
+      <meta name="description" content="Veatfresh is quality conscious online delivery brand for convenient supply of fresh and chemical free meat products including seafood, poultry products-Chicken, Goat meat(Mutton) and also organic fresh fruits"/>
+      <meta property="og:title" content="">
+      <meta property="og:type" content="">
+      <meta property="og:url" content="">
+      <meta property="og:image" content="">
+       <!-- Favicon -->
+        <!--<link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/imgs/theme/favicon.png');?>">-->
+      <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/mainc619.css'); ?>">
+      <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/plugins/owl-carousel/owl.carousel.css'); ?>">
+      <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/plugins/owl-carousel/owl.theme.css'); ?>">
+      
+      
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('assets/imgs/theme//apple-icon-57x57.png');?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('assets/imgs/theme//apple-icon-60x60.png');?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('assets/imgs/theme//apple-icon-72x72.png');?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/imgs/theme//apple-icon-76x76.png');?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('assets/imgs/theme//apple-icon-114x114.png');?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('assets/imgs/theme//apple-icon-120x120.png');?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('assets/imgs/theme//apple-icon-144x144.png');?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('assets/imgs/theme//apple-icon-152x152.png');?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/imgs/theme//apple-icon-180x180.png');?>">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url('assets/imgs/theme//android-icon-192x192.png');?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/imgs/theme//favicon-32x32.png');?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets/imgs/theme//favicon-96x96.png');?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/imgs/theme//favicon-16x16.png');?>">
+    <link rel="manifest" href="<?= base_url('assets/imgs/theme//manifest.json');?>">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= base_url('/ms-icon-144x144.png');?>">
+    <meta name="theme-color" content="#ffffff">
+</head>
